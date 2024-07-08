f8d779fdccba08d27b519aebe01e12efd80d0ca2
jinalparikh25 <jinalparikh2595@gmail.com>
2024-07-08 13:35:57 +0200

    Get Movies list, Genre table, get customer list and deatils using Entity framework 1st code approch and Migration

--- Music_Store_Management/Music_Store_Management/Controllers/CustomerController.cs
+++ Music_Store_Management/Music_Store_Management/Controllers/CustomerController.cs
@@ -2,24 +2,30 @@
 using Music_Store_Management.Models;
 using Music_Store_Management.Services;
 using Music_Store_Management.ViewModels;
+using Music_Store_Management.context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Music_Store_Management.Controllers
 {
     public class CustomerController : Controller
     {
-        RandomViewModel randomViewModel = new RandomViewModel();
-        GetCustomerListServices getCustomerListServices = new GetCustomerListServices();
+        private MusicStoreContext _context;
+
+        public CustomerController(MusicStoreContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            randomViewModel.CustomerList = getCustomerListServices.GetCustomers();
+            List<Customer> customer_List = _context.Customers.Include(c => c.MembershipType).ToList();
            
-            return View(randomViewModel);
+            return View(customer_List);
         }
 
         [Route("Details/{id}")]
         public IActionResult Details(int id)
         {
-                Customer customer = getCustomerListServices.GetCustomers().FirstOrDefault(x => x.Id == id);
+                Customer customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(x => x.Id == id);
 
                 if (customer == null)
                 {
---
+++ Music_Store_Management/Music_Store_Management/Models/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace Music_Store_Management.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
