e34bbceec2a3612d72e0cf80b55176a5c2fc7960
jinalparikh25 <jinalparikh2595@gmail.com>
2024-07-08 19:29:30 +0200

    Save and Update Customer and Movie using EntityFramework

--- Music_Store_Management/Music_Store_Management/Migrations/20240708134002_changeByteToIntMembershipId.Designer.cs
+++ Music_Store_Management/Music_Store_Management/Migrations/20240708134002_changeByteToIntMembershipId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Music_Store_Management.context;
 
@@ -11,9 +12,11 @@ using Music_Store_Management.context;
 namespace Music_Store_Management.Migrations
 {
     [DbContext(typeof(MusicStoreContext))]
-    partial class MusicStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240708134002_changeByteToIntMembershipId")]
+    partial class changeByteToIntMembershipId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
